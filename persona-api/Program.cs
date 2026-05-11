5827fd9b7190e858c8b0e11559b6e4dded8681ac
gyusba <f-garrido@javeriana.edu.co>
2026-05-10 21:31:22 -0500

    fix

--- persona-api/Program.cs
+++ persona-api/Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddScoped<IEstudioRepository, EstudioRepository>();
 builder.Services.AddScoped<IPersonaRepository, PersonaRepository>();
 builder.Services.AddScoped<ITelefonoRepository, TelefonoRepository>();
 
-builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
